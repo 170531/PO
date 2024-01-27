c63c3bf6678a5d21cfd0e62a506f6ddccb4c9ab4
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:22:54 +0100

    Add files via upload

---
+++ zadanie 2.12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wprowadzaj liczby całkowite. Wprowadź 0 (zero), aby zakończyć.");
+
+        int suma = 0;
+        int liczba;
+
+        do
+        {
+            Console.Write("Podaj liczbę: ");
+            liczba = Convert.ToInt32(Console.ReadLine());
+            suma += liczba;
+        } while (liczba != 0);
+
+        Console.WriteLine($"Suma wprowadzonych liczb: {suma}");
+    }
+}
