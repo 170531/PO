6c1fa74a7e2eb89d08234884ebda78ab5e5f0e91
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 11:58:56 +0100

    Add files via upload

---
+++ zadanie 2.1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj rok do sprawdzenia: ");
+        int rok = Convert.ToInt32(Console.ReadLine());
+
+        if (CzyPrzestepny(rok))
+        {
+            Console.WriteLine($"{rok} to rok przestępny.");
+        }
+        else
+        {
+            Console.WriteLine($"{rok} nie jest rokiem przestępnym.");
+        }
+    }
+
+    static bool CzyPrzestepny(int rok)
+    {
+        
+        return (rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0);
+    }
+}
