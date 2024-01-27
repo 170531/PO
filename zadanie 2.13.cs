a7efa5ecf75e638bec2ed661cd90ba184f13cd9c
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:24:01 +0100

    Add files via upload

---
+++ zadanie 2.13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę naturalną n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int suma = ObliczSumeSzeregu(n);
+
+        Console.WriteLine($"Suma szeregu W({n}) = {suma}");
+    }
+
+    static int ObliczSumeSzeregu(int n)
+    {
+        int suma = 0;
+        int znak = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            suma += znak * i;
+            znak *= -1; 
+        }
+
+        return suma;
+    }
+}
