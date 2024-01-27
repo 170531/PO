d7b86991734d0fb1053b671428010e0f4a013190
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 11:48:25 +0100

    Add files via upload

---
+++ zadanie 1.1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj temperaturę w stopniach Celsjusza: ");
+        double temperaturaCelsjusza = Convert.ToDouble(Console.ReadLine());
+
+        double temperaturaFahrenheita = PrzeliczNaFahrenheita(temperaturaCelsjusza);
+
+        Console.WriteLine($"Temperatura w stopniach Fahrenheita: {temperaturaFahrenheita} °F");
+    }
+
+    static double PrzeliczNaFahrenheita(double temperaturaCelsjusza)
+    {
+        return (temperaturaCelsjusza * 9 / 5) + 32;
+    }
+}
