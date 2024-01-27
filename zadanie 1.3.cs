e12e294ebbc372ac0fd928d3829003a979022059
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 11:55:57 +0100

    Add files via upload

---
+++ zadanie 1.3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program do obliczania wskaźnika masy ciała (BMI)");
+
+
+        Console.Write("Podaj wagę (w kilogramach): ");
+        double waga = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj wzrost (w metrach): ");
+        double wzrost = Convert.ToDouble(Console.ReadLine());
+
+
+        double bmi = ObliczBMI(waga, wzrost);
+
+
+        Console.WriteLine($"Twoje BMI wynosi: {bmi}");
+
+
+
+        static double ObliczBMI(double waga, double wzrost)
+        {
+
+            return waga / (wzrost * wzrost);
+        }
+
+
+    }
+}
+
