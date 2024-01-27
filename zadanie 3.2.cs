9811fded7f6555832b76907038635ab7bd196065
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:29:25 +0100

    Add files via upload

---
+++ zadanie 3.2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] tab1 = { 3, -5, 8, -2, 1, 0, -4, 7, -1, 6 };
+        int[] tab2 = new int[10];
+
+        KopiujDodatnie(tab1, tab2);
+
+        Console.WriteLine("Tablica tab1:");
+        WyswietlTablice(tab1);
+
+        Console.WriteLine("\nTablica tab2 (wartości dodatnie z tab1):");
+        WyswietlTablice(tab2);
+    }
+
+    static void KopiujDodatnie(int[] tablicaWejsciowa, int[] tablicaWyjsciowa)
+    {
+        for (int i = 0; i < tablicaWejsciowa.Length; i++)
+        {
+            if (tablicaWejsciowa[i] > 0)
+            {
+                tablicaWyjsciowa[i] = tablicaWejsciowa[i];
+            }
+        }
+    }
+
+    static void WyswietlTablice(int[] tablica)
+    {
+        foreach (var element in tablica)
+        {
+            Console.Write($"{element} ");
+        }
+        Console.WriteLine();
+    }
+}
