9a52f20338d7637f6f1f568017e6628b283a27a0
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:01:58 +0100

    Add files via upload

---
+++ zadanie 2.3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj pierwszą liczbę: ");
+        double liczba1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj drugą liczbę: ");
+        double liczba2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj trzecią liczbę: ");
+        double liczba3 = Convert.ToDouble(Console.ReadLine());
+
+        double najwiekszaLiczba = ZnajdzNajwiekszaLiczbe(liczba1, liczba2, liczba3);
+
+        Console.WriteLine($"Największa liczba to: {najwiekszaLiczba}");
+    }
+
+    static double ZnajdzNajwiekszaLiczbe(double a, double b, double c)
+    {
+        
+        return Math.Max(a, Math.Max(b, c));
+    }
+}
