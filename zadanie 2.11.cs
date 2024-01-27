abc56f8b81ef1554fd359668389c036f5a58242b
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:21:46 +0100

    Add files via upload

---
+++ zadanie 2.11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int suma = 0;
+        int liczba = 1;
+        int iloscLiczb = 0;
+
+        while (suma <= 100)
+        {
+            suma += liczba;
+            liczba++;
+            iloscLiczb++;
+        }
+
+        Console.WriteLine($"Ilość kolejnych liczb dodanych do siebie, aby suma przekroczyła 100: {iloscLiczb}");
+    }
+}
