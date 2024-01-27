404493853c148d6408f05d650c587c979137f358
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:20:13 +0100

    Add files via upload

---
+++ zadanie 2.10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("Silnia nie jest zdefiniowana dla liczb ujemnych.");
+        }
+        else
+        {
+            long wynik = ObliczSilnie(n);
+            Console.WriteLine($"{n}! = {wynik}");
+        }
+    }
+
+    static long ObliczSilnie(int liczba)
+    {
+        if (liczba == 0 || liczba == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return liczba * ObliczSilnie(liczba - 1);
+        }
+    }
+}
