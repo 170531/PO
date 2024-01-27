16206cb00db94b4efc660521cadc8ecd8222bc35
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:25:16 +0100

    Add files via upload

---
+++ zadanie 2.14.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Liczby doskonałe w przedziale od 1 do {n}:");
+        ZnajdzLiczbyDoskonale(n);
+    }
+
+    static void ZnajdzLiczbyDoskonale(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (CzyDoskonala(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static bool CzyDoskonala(int liczba)
+    {
+        int sumaDzielnikow = 0;
+
+        for (int i = 1; i < liczba; i++)
+        {
+            if (liczba % i == 0)
+            {
+                sumaDzielnikow += i;
+            }
+        }
+
+        return sumaDzielnikow == liczba;
+    }
+}
