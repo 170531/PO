e56f16c762eb17c8fef7069128941ae094ef844c
170531 <122684556+170531@users.noreply.github.com>
2024-01-27 12:28:05 +0100

    Add files via upload

---
+++ zadanie 3.1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę elementów tablicy: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] tablica = new int[n];
+
+        WypelnijTablice(tablica);
+
+        Console.WriteLine("Tablica po wypełnieniu:");
+        WyswietlTablice(tablica);
+    }
+
+    static void WypelnijTablice(int[] tablica)
+    {
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.Write($"Podaj wartość dla elementu {i + 1}: ");
+            tablica[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    static void WyswietlTablice(int[] tablica)
+    {
+        foreach (var element in tablica)
+        {
+            Console.Write($"{element} ");
+        }
+        Console.WriteLine();
+    }
+}
